ccf45c3dacaf4857f6eabe24cf36badb610aa5d7
Gabriel Honda <gabrielhondacba@gmail.com>
2025-03-29 21:11:17 -0400

    chore: commit inicial

---
+++ src/test/java/br/com/ghonda/ProjetoPraticoEdital0022025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ghonda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPraticoEdital0022025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
